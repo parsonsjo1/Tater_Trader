a3442b31edb98ac10d96a673e7499020f62eb118
Joshua <jopa.d.parsons@gmail.com>
2016-04-10 23:04:28 -0600

    Login and Register are working. BarcodeScanner is working with upc api :)

--- app/src/main/java/com/joshwa/tater_trader/ServerProxy.java
+++ app/src/main/java/com/joshwa/tater_trader/ServerProxy.java
@@ -4,6 +4,7 @@ import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import com.google.gson.reflect.TypeToken;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -270,6 +271,146 @@ public class ServerProxy
             e.printStackTrace();
         }
 
+        storeUPCInfo(upcInfo);
+
         return upcInfo;
+    }
+
+    private void storeUPCInfo(UPCInfo upcInfo)
+    {
+        try
+        {
+            url = new URL("http://ec2-52-90-159-103.compute-1.amazonaws.com:3010/upc");
+        } catch (MalformedURLException e)
+        {
+            e.printStackTrace();
+        }
+
+        HttpURLConnection connection = null;
+        JSONObject jsonParam = new JSONObject();
+        try
+        {
+            jsonParam.put("productname", upcInfo.getProductName());
+            jsonParam.put("imageurl", upcInfo.getImageURL());
+            jsonParam.put("producturl", upcInfo.getProductURL());
+            jsonParam.put("price", upcInfo.getPrice());
+            jsonParam.put("currency", upcInfo.getCurrency());
+            jsonParam.put("saleprice", upcInfo.getSaleprice());
+            jsonParam.put("storename", upcInfo.getStorename());
+
+        }
+        catch (JSONException e)
+        {
+            e.printStackTrace();
+        }
+
+        try
+        {
+            connection = (HttpURLConnection) url.openConnection();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        try
+        {
+            connection.setRequestMethod("POST");
+        } catch (ProtocolException e)
+        {
+            e.printStackTrace();
+        }
+        //connection.setRequestProperty("USER-AGENT", "Mozilla/5.0");
+        //connection.setRequestProperty("ACCEPT-LANGUAGE", "en-US,en;0.5");
+        connection.setDoOutput(true);
+        connection.setUseCaches(false);
+        connection.setConnectTimeout(10000);
+        connection.setReadTimeout(10000);
+        connection.setRequestProperty("Content-Type", "application/json");
+
+        int responseCode = 0; // getting the response code
+        try
+        {
+            connection.connect();
+            OutputStreamWriter out = new OutputStreamWriter(connection.getOutputStream());
+            out.write(jsonParam.toString());
+            out.close();
+            responseCode = connection.getResponseCode();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        BufferedReader br = null;
+        try
+        {
+            br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        Gson gson = new Gson();
+        JSONResponse jsonResponse = gson.fromJson(br, JSONResponse.class);
+        System.out.println("JSON RESPONSE: " +  jsonResponse.getMessage());
+    }
+
+    public List<UPCInfo> getUPCInfo()
+    {
+        try
+        {
+            url = new URL("http://ec2-52-90-159-103.compute-1.amazonaws.com:3010/upc");
+        } catch (MalformedURLException e)
+        {
+            e.printStackTrace();
+        }
+
+        HttpURLConnection connection = null;
+
+        try
+        {
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+        }
+        catch (ProtocolException e)
+        {
+            e.printStackTrace();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+
+        connection.setRequestProperty("Content-length", "0");
+        connection.setUseCaches(false);
+        connection.setAllowUserInteraction(false);
+        connection.setConnectTimeout(10000);
+        connection.setReadTimeout(10000);
+
+        int responseCode = 0; // getting the response code
+        try
+        {
+            connection.connect();
+            responseCode = connection.getResponseCode();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+
+        BufferedReader br = null;
+        try
+        {
+            br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+
+        JSONObject json = null;
+        Gson gson = new Gson();
+        List<UPCInfo> upcInfoList = gson.fromJson(br, new TypeToken<List<UPCInfo>>(){}.getType());
+
+        return upcInfoList;
     }
 }
